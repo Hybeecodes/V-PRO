d2688550e874ecc8346c525df0b66fad92fa74a2
megastar98 <obikoya11@gmail.com>
2018-04-25 07:14:05 +0100

    latest

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,20 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const School = require('../models/School');
 const multer = require('multer');
+const { check, validationResult } = require('express-validator/check');
+const bcrypt = require('bcrypt-nodejs');
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '/uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+})
+ 
+var upload = multer({ storage: storage })
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -18,14 +32,15 @@ router.get('/current_user',(req,res,next)=>{
   }
 })
 
+
 router.get('/login',(req,res,next) =>{
-  res.render('login');
+  res.render('login',{title:'Login'});
 });
 
 
 
 router.get('/register',(req,res,next) =>{
-  res.render('register');
+  res.render('register',{title:'Register'});
 })
 
 router.get('/register_student',(req,res,next) => {
@@ -58,7 +73,7 @@ router.post('/login',(req,res,next)=>{
   }
   // console.log(req.body)
     let email = req.body.email;
-    let password = req.body.password;
+    let password =bcrypt.hashSync(req.body.password);
     // // console.log(password);
     School.findOne({email:email,password:password},(err,school)=>{
       if(err){
@@ -68,31 +83,48 @@ router.post('/login',(req,res,next)=>{
         res.json({status:0,message:"Sorry,Invalid Email or Password"});
       }else{
         //set school session
-        req.session.user_session = school._id;
+        req.session.user_session = school;
         res.json({status:1,message:school});
       }
     });
 });
 
-router.post('/school',(req,res,next)=>{
+// register new school
+router.post('/register',(req,res,next)=>{
   // check if all required fields are sent
   // if(req.files)/
+  console.log(req.body)
   if(req.body){
     if(req.body.name == undefined || req.body.address == undefined||req.body.email == undefined||req.body.password == undefined||req.body.phone == undefined || req.file == undefined){
       res.json({status:0,message:"Sorry, One or more credentials missing"});
     }else{
-      School.save((err,school)=>{
+      upload.single('photo',(err,photo)=>{
         if(err){
-          res.json({status:0,message:"Sorry,Unable to register new Scholl"});
+          res.json({status:0,message:"Sorry,Unable to upload photo"});
         }else{
-          res.json({status:1,message:school});
+          const newSchool = {
+            name: req.body.name,
+            address: req.body.address,
+            email:req.body.email,
+            password: bcrypt.hashSync(req.body.password),
+            phone: req.body.phone
+          }
+          School.save(newSchool,(err,school)=>{
+            if(err){
+              res.json({status:0,message:"Sorry,Unable to register new Scholl"});
+            }else{
+              res.json({status:1,message:school});
+            }
+          }) 
         }
-      }) 
+      })
     }
   }else{
     res.json({status:0,message:"Sorry, request body is empty"});
   }
 });
+
+
 
 
 
