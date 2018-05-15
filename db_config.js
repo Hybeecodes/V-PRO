d79143989240052c5425845a450f5ce90987db78
megastar98 <obikoya11@gmail.com>
2018-05-15 23:12:45 +0100

    Moved to online db

--- db_config.js
+++ db_config.js
@@ -1,7 +1,8 @@
 // import { mongo } from 'mongoose';
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/v-pro',function(err){
+require('dotenv').config();
+mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_USER}@ds119250.mlab.com:19250/v-pro`,function(err){
     if(err){
         throw err;
     }else{
